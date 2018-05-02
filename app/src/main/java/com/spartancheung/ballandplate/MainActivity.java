9ec879634075db65e6f61de6f5613bb7448fa563
SpartanCheungSurface <zd853768760@gmail.com>
2018-05-02 09:20:43 +0800

    App for Ball and Plate System
    实现了动态读取蓝牙4.0数据并实时作图的效果，但是存在作图缓慢的问题，越来越卡顿

---
+++ app/src/main/java/com/spartancheung/ballandplate/MainActivity.java
@@ -0,0 +1,84 @@
+package com.spartancheung.ballandplate;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //SectionPager myPagerAdapter = new SectionPager(getSupportFragmentManager());
+        //ViewPager viewPager = (ViewPager) findViewById(R.id.viewpage);
+        //viewPager.setAdapter(myPagerAdapter);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public class SectionPager extends FragmentPagerAdapter {
+        private SectionPager (FragmentManager fm){
+            super(fm);
+        }
+
+        public int getCount() {
+            return 3;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch(position) {
+                case 0:
+                    return new MainFragment();
+                case 1:
+                    return new ControlCenter();
+                case 2:
+                    return new GraphFragment();
+            }
+            return null;
+        }
+    }
+}
